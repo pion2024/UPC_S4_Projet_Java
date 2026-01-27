8fb50f7a9943dd5c18b0649399156fb0ec8de9c5
pinyuan.chen <pionchan2024@outlook.com>
2026-01-27 21:35:53 +0100

    add notes in Launcher.java

--- src/main/java/com/starstuff/Launcher.java
+++ src/main/java/com/starstuff/Launcher.java
@@ -5,6 +5,11 @@ public class Launcher {
     // utilise la commande suivante d'abord pour introduire javafx :
     // mvn clean javafx:run
     // ensuite executer ce Laucher.java
+
+    // Note pour la premier essai: 
+    // l'agent face à la direction dans la quelle il se déplace.
+    // donc pour prendre un bloc, il faut d'abord déplacer l'agent dans cette direction 
+    // pour lui faire facer au bloc, voir la demo.
     public static void main(String[] args) {
         App.main(args); 
     }
