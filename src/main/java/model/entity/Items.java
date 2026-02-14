88ca1ec487a2b0aa17d28c21477e09c5e37ccadb
Bouricha-fatima <fatima.bouricha@etu.u-paris.fr>
2026-02-14 18:01:14 +0100

    modification de la classe Items avec ajout d'attributs et méthodes

--- src/main/java/model/entity/Items.java
+++ src/main/java/model/entity/Items.java
@@ -1,5 +1,23 @@
 package model.entity;
 
-public class Items {
-    
+public class Items extends Entity{
+    /* classe abstraite dont héritera tout les objets a stocker dans la matrice */
+
+    protected boolean traversable;
+    protected int id; // numéro unique pour chaque instance -> utile pour activer un pont spécifique ...
+
+
+    public Items(int id, int x, int y, boolean traversable){
+        super(x,y);
+        this.traversable = traversable;
+        this.id = id;
+    }
+
+    public boolean isTraversable(){
+        return traversable;
+    }
+
+    public int getId(){
+        return this.id;
+    }
 }
