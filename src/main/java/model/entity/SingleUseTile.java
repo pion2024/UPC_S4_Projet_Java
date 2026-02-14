d380fe2629a31b5774b023eb43d4fb05895b6b5f
Bouricha-fatima <fatima.bouricha@etu.u-paris.fr>
2026-02-14 22:29:12 +0100

    ajout de la classe SingleUseTile ( Dalle a usage unique )

---
+++ src/main/java/model/entity/SingleUseTile.java
@@ -0,0 +1,18 @@
+package model.entity;
+
+// La dalle qui se casse après le passage
+public class SingleUseTile extends Items {
+    private boolean broken = false;
+
+    public SingleUseTile(int id,int x, int y) {
+        super(id, x, y, true);
+    }
+
+    @Override
+    public void onSteppedOn(Entity stepper) {
+        if (!broken) {
+            this.broken = true;
+            this.traversable = false; // La dalle s'est effondrée 
+        }
+    }
+}
