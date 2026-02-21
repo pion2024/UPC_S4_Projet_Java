32704525f86d3b7e578c5bfd3bb7b667fbba8bbd
Bouricha-fatima <fatima.bouricha@etu.u-paris.fr>
2026-02-21 22:52:37 +0100

    ajout d'une classe propulseur

---
+++ src/main/java/model/entity/Propulsor.java
@@ -0,0 +1,24 @@
+package model.entity;
+import model.physic.*;
+
+public class Propulsor extends Items {
+
+    private final Direction propulsDirection;
+
+    public Propulsor(int id, Position pos, Direction dir){
+        super(id, pos, true, CellType.PROPULSOR);
+        this.propulsDirection = dir;
+    }
+
+    @Override
+    public void onSteppedOn(Entity e){
+        int newX = e.getPos().getX() + propulsDirection.getDx();
+        int newY = e.getPos().getY() + propulsDirection.getDy();
+        e.getPos().setX(newX);
+        e.getPos().setY(newY);
+    }
+
+    public Direction getpropulsDirection(){
+        return propulsDirection;
+    }
+}
