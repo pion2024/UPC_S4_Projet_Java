4955f5dbb558ccb722a296a224bdb3b7a8352451
Bouricha-fatima <fatima.bouricha@etu.u-paris.fr>
2026-03-03 14:49:13 +0100

    ajout de toggle dans propulseur

--- src/main/java/model/entity/Propulsor.java
+++ src/main/java/model/entity/Propulsor.java
@@ -1,13 +1,19 @@
 package model.entity;
 import model.physic.*;
 
-public class Propulsor extends Items {
+public class Propulsor extends Items implements Activatable{
 
     private final Direction propulsDirection;
+    private boolean activate;
 
     public Propulsor(int id, Position pos, Direction dir){
         super(id, pos, true, CellType.PROPULSOR, dir);
         this.propulsDirection = dir;
+    }
+
+    @Override
+    public void toggle(){
+        this.activate = true;
     }
 
     @Override
@@ -16,6 +22,7 @@ public class Propulsor extends Items {
         int newY = e.getPos().getY() + propulsDirection.getDj();
         e.getPos().setX(newX);
         e.getPos().setY(newY);
+        toggle();
     }
 
     public Direction getpropulsDirection(){
