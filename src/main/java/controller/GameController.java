cc942bb88dccdcf0bb7e3582660c54625b7b3276
Bouricha-fatima <fatima.bouricha@etu.u-paris.fr>
2026-03-09 17:29:46 +0100

    ajout d'une méthode qui lit le niveau choisi et prépare le niveau pour le lancer

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -3,34 +3,51 @@ package controller;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import javax.swing.JFrame;
+
 import model.GameModel;
+import model.Level;
 import model.physic.Direction;
 import model.physic.MovementManager;
 import view.GameView;
+import view.WindowManager;
 
 public class GameController extends KeyAdapter {
     private GameModel model;
     private GameView view;
+    private WindowManager window;
     private MovementManager moveMgr;
-    private JFrame frame;
 
-    public GameController(GameModel model, GameView view) {
-        this.model = model;
-        this.view = view;
-        this.moveMgr = new MovementManager(model.getBoard());
+    public GameController(WindowManager window) {
+        this.window = window;
+        this.view = window.getGameView();
+        // On attache l'écouteur de touches au conteneur principal géré par le WindowManager
+        window.getContainer().addKeyListener(this);
 
-        frame = new JFrame("VP2C");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.add(view);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.addKeyListener(this);
     }
 
-    public void start() {
-        frame.setVisible(true);
+    public void prepareLevel(int levelNum) {
+        // On récupère la configuration dans l'Enum Level
+        Level[] allLevels = Level.values();
+    
+        // Si on demande un niveau qui n'est pas encore codé dans l'Enum
+        if (levelNum > allLevels.length) {
+            System.out.println("Niveau non implémenté.");
+            return; 
+        }
+
+        Level selectedLevel = allLevels[levelNum - 1];
+
+        //On crée un nouveau modèle propre pour ce niveau
+        this.model = new GameModel(selectedLevel.getWidth(), selectedLevel.getHeight());
+        
+        //On demande au Level de remplir le modèle (îles, ponts, robot...)
+        selectedLevel.setup(this.model);
+
+        //On branche la vue sur ce nouveau modèle
+        this.view.setModel(model);
+        
+        //On initialise le moteur de mouvement pour ce nouveau plateau
+        this.moveMgr = new MovementManager(model.getBoard());
         view.repaint();
     }
 
