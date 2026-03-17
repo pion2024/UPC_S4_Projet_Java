8866ff4f6fb8360e0c59d8dea8d7113201dbe577
CHEN David <david.chen1@etu.u-paris.fr>
2026-03-17 12:15:24 +0100

    Position.java: Renommé les attributs `(x, y) -> (i, j)` .

--- src/main/java/model/physic/Position.java
+++ src/main/java/model/physic/Position.java
@@ -1,20 +1,20 @@
 package model.physic;
 
 public class Position {
-    private int x;
-    private int y;
+    private int i;
+    private int j;
 
-    public Position(int x, int y){
-        this.x = x;
-        this.y = y;
+    public Position(int j, int i){
+        this.i = i;
+        this.j = j;
     }
 
     /* ----------- getteurs / setteurs ----------- */
 
-    public int getX(){ return this.x; }
-    public int getY(){ return this.y; }
-    public void setX(int x){ this.x = x; }
-    public void setY(int y){ this.y = y; }
+    public int getJ(){ return this.j; }
+    public int getI(){ return this.i; }
+    public void setJ(int j){ this.j = j; }
+    public void setI(int i){ this.i = i; }
 
     // equals méthode qui permet de savoir si deux objets sont sur la même case 
 
@@ -22,7 +22,7 @@ public class Position {
     public boolean equals(Object o){
         if(this == o) return true; // c'est le même objet
         if(o instanceof Position p) { // compare les positions des objets
-            return this.x == p.getX() && this.y == p.getY();
+            return this.j == p.getJ() && this.i == p.getI();
         }
         return false;
     }
