36104064a8ad3d911285da7a9bbe23c1fede9ea9
michel ngy <michel.ngy@etu.u-paris.fr>
2026-03-24 12:11:12 +0100

    ajout des textures dans les ressources/assets  (les textures n'ont pas encore étaient implémenté en jeu)

--- src/main/java/view/AssetManager.java
+++ src/main/java/view/AssetManager.java
@@ -9,9 +9,19 @@ import javax.imageio.ImageIO;
 public class AssetManager {
     public static BufferedImage BACKGROUND;
     public static BufferedImage LOGO; // Sera null car pas encore d'image
+    public static BufferedImage PLAYER;
+    public static BufferedImage GROUND;
+    public static BufferedImage SWITCH;
+    public static BufferedImage CLOSED_BRIDGE;
+    public static BufferedImage OPENED_BRIDGE;
 
     public static void loadAssets() {
         BACKGROUND = loadImage("/assets/background.png");
+        PLAYER = loadImage("/assets/player.png");
+        GROUND = loadImage("/assets/ground_block.png");
+        SWITCH = loadImage("/assets/block_switch.png");
+        CLOSED_BRIDGE = loadImage("/assets/closed_bridge.png");
+        OPENED_BRIDGE = loadImage("/assets/opened_bridge.png");
     }
 
     private static BufferedImage loadImage(String path) {
@@ -27,4 +37,34 @@ public class AssetManager {
             return null;
         }
     }
+
+
+
+    // public void render(Graphics2D g, GameModel model) {
+    //     //Sol
+    //     for (Ground ground : model.getGrounds()) {
+    //         if (GROUND != null)
+    //             g.drawImage(GROUND, ground.getI(), ground.getJ(), null);
+    //         else
+    //             g.fillRect(ground.getI(), ground.getJ(), 32, 16);
+    //     }
+
+    //     //Joueur
+    //     Robot p = model.getPlayer();
+    //     if (PLAYER != null)
+    //         g.drawImage(PLAYER, p.getI(), p.getJ(), null);
+
+    //     //Pont : la VIEW choisit l'image selon l'état logique du MODEL
+    //     for (Bridge bridge : model.getBridges()) {
+    //         BufferedImage img = bridge.getIsActive() ? OPENED_BRIDGE : CLOSED_BRIDGE;
+    //         if (img != null)
+    //             g.drawImage(img, bridge.getI(), bridge.getJ(), null);
+    //     }
+
+    //     //Switch
+    //     for (Switch sw : model.getSwitches()) {
+    //         if (SWITCH != null)
+    //             g.drawImage(SWITCH, sw.getI(), sw.getJ(), null);
+    //     }
+    // }
 }
