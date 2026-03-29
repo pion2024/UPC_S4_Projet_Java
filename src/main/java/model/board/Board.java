c51f4c5190d8c89282125f93c4cb20142719984a
CHEN David <david.chen1@etu.u-paris.fr>
2026-03-29 18:10:15 +0200

    Board.java: La classe `Board` n'étend plus `Matrix<Items>`, et son champ de type tableau de tableaux de `Items` (Items[][]) (champ qu'elle héritait par `Matrix<Items>`) est renommé de `elements -> items`.

--- src/main/java/model/board/Board.java
+++ src/main/java/model/board/Board.java
@@ -6,7 +6,7 @@ import java.util.List;
 import model.entity.Items;
 import model.entity.MovableEntity;
 
-public class Board extends Matrix<Items>{
+public class Board {
     /* Attributs de classe */
     // Constantes
 
@@ -16,11 +16,22 @@ public class Board extends Matrix<Items>{
     // Constantes
 
     // Variables
+    protected int nbLines;      // nbLines      doit être > 0
+    protected int nbColumns;    // nbLColumns   doit être > 0
+    protected Items[][] items;
     private List<MovableEntity> movableEntities; // (Blocs, Robots, Joueur)
 
     /* Constructeurs */
-    public Board(Matrix<Items> items) {
-        super(items.elements);
+    public Board(int nbLines, int nbColumns) {
+        this.nbLines = nbLines;
+        this.nbColumns = nbColumns;
+        this.items = new Items[nbLines][nbColumns];
+    }
+
+    public Board(Items[][] items) {
+        this.nbLines = items.length;
+        this.nbColumns = items[0].length;
+        this.items = items;
         this.movableEntities = new ArrayList<>();
     }
 
@@ -28,11 +39,37 @@ public class Board extends Matrix<Items>{
 
     /* Méthodes dynamiques */
     // Getters
+    public int getNbColumns() {
+        return this.nbColumns;
+    }
+
+    public int getNbLines() {
+        return this.nbLines;
+    }
+
+    public Items[][] getItems() {
+        return this.items;
+    }
+
+    public Items getItemAt(int lineIndex, int columnIndex) {
+        if ( !this.isInside(lineIndex, columnIndex) ) {
+            throw new IllegalArgumentException("Les indices sont hors bornes.");
+        }
+        return this.items[lineIndex][columnIndex];
+    }
+
     public List<MovableEntity> getMovableEntities() { 
         return this.movableEntities; 
     }
 
     // Setters
+    public void setItem(int lineIndex, int columnIndex, Items item) {
+        if ( !this.isInside(lineIndex, columnIndex) ) {
+            throw new IllegalArgumentException("Les indices sont hors bornes.");
+        }
+        this.items[lineIndex][columnIndex] = item;
+    }
+
     public void setMovableEntities(List<MovableEntity> movableEntities) {
         this.movableEntities = movableEntities;
     }
@@ -42,6 +79,17 @@ public class Board extends Matrix<Items>{
     /* Méthodes protégées */
 
     /* Méthodes publiques */
+    /**
+     * Permet de vérifier si une position donnée est dans la matrice.
+     * @param lineIndex
+     * @param columnIndex
+     * @return
+     */
+    public boolean isInside(int lineIndex, int columnIndex) {
+        return  (0 <= lineIndex && lineIndex < this.nbLines) &&
+                (0 <= columnIndex && columnIndex < this.nbColumns);
+    }
+
     /**
      * Utilitaire pour trouver s'il y a un objet mobile à une position donnée.
      * @param x
